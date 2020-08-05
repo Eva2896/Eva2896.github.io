cb17fc3d0b16803e9971198a9d2ac6dbe55bab6a
Eva2896 <eva_paatricia@hotmail.com>
2020-08-04 22:17:19 -0300

    Trabajo final introducción a la programación

--- validation.js
+++ validation.js
@@ -1,7 +1,17 @@
-const form = document.querySelector("#info")
-form.addEventListener("submit" ,(event)=>{
-event.preventDefault()
-console.log("trato de enviar el mensaje")	
+const submit = document.querySelector("#submit")
+submit.addEventListener("click", function(event) {
+  event.preventDefault();
+  const email = document.querySelector("#email").value;
+  const emailHelp = document.querySelector("#emailHelp");
 
-})
+  console.log(email);
 
+  if(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/.test(email)) {
+    emailHelp.textContent = "Email Enviado!";
+  } 
+
+  if(!/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/.test(email)) {
+    emailHelp.textContent = "Email erroneo!";
+  } 
+
+});
